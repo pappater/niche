2655b54e04aeb9dcc319a41c232400c6187b44a5
pappater <pratheesheswaran@gmail.com>
2024-09-16 23:23:02 +0530

    removed try catch

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -8,25 +8,17 @@ import { Quote } from "@/types";
 let cachedQuotes: Quote[][] | null = null;
 
 export async function GET(request: NextRequest) {
-  try {
-    const page = parseInt(request.nextUrl.searchParams.get("page") || "1");
+  const page = parseInt(request.nextUrl.searchParams.get("page") || "1");
 
-    if (!cachedQuotes) {
-      const filePath = path.join(process.cwd(), "public", "quotes.json");
-      const fileContents = await fs.readFile(filePath, "utf8");
-      console.log("File Path:", filePath);
-      console.log("File Contents:", fileContents);
-      cachedQuotes = JSON.parse(fileContents);
-    }
-
-    const quotes = cachedQuotes![page - 1];
-
-    return NextResponse.json({ quotes, hasMore: page < cachedQuotes!.length });
-  } catch (error) {
-    console.error("Error in quotes API:", error);
-    return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 }
-    );
+  if (!cachedQuotes) {
+    const filePath = path.join(process.cwd(), "public", "quotes.json");
+    const fileContents = await fs.readFile(filePath, "utf8");
+    console.log("File Path:", filePath);
+    console.log("File Contents:", fileContents);
+    cachedQuotes = JSON.parse(fileContents);
   }
+
+  const quotes = cachedQuotes![page - 1];
+
+  return NextResponse.json({ quotes, hasMore: page < cachedQuotes!.length });
 }
