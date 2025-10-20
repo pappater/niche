c8e2e6f3d313f4c836fdbf10acdd323e958c0286
Pratheesh <58771724+pappater@users.noreply.github.com>
2025-10-20 15:23:14 +0000

    fixed 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const repoName = "niche";
 const nextConfig = {
   images: {
     domains: ["www.nitch.com"],
     unoptimized: true, // Disable Image Optimization for static export
   },
   output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
